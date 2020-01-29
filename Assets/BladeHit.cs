86786e8a3ee6731296f9f4946b4f988b00501ee2
Alexandrealvesr <33659524+Alexandrealvesr@users.noreply.github.com>
2020-01-29 20:00:40 +0100

    Sounds ground

--- Assets/BladeHit.cs
+++ Assets/BladeHit.cs
@@ -7,10 +7,17 @@ public class BladeHit : MonoBehaviour
 {
     SteamVR_Input_Sources hand;
     public SteamVR_Action_Vibration haptic;
+
+    public AudioClip hitZombie;
+
+    public List<AudioClip> hitGround;
+
+    private AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         hand = SteamVR_Input_Sources.RightHand;
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -20,7 +27,24 @@ public class BladeHit : MonoBehaviour
     }
 
     void OnCollisionEnter (Collision collision) {
-        this.gameObject.GetComponent<AudioSource>().Play();
-        haptic.Execute(0f, 0.15f, 100f, 1f, hand);
+
+        float magnitude = Mathf.Min(collision.relativeVelocity.magnitude  / 100, 1) ;
+        Debug.Log("HIT MAGNITUDE : " + magnitude);
+        if(collision.gameObject.name.Contains("Zombie"))
+        {
+            audioSource.clip = hitZombie;
+        }
+        else
+        {
+            if(hitGround != null && hitGround.Count > 0)
+            {
+                
+                audioSource.clip = hitGround[Random.Range(0,hitGround.Count - 1)];
+            }
+            //Else ne devrait pas arriver, bsx
+        }
+        audioSource.volume =magnitude;
+        audioSource.Play();
+        haptic.Execute(0f, 0.15f, 100f, magnitude , hand);
     }
 }
