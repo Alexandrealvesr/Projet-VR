11ab8c542776b0f5ed0316bee7c145b2d49050bd
troncoli <thomas.roncoli@gmail.com>
2020-01-30 14:51:52 +0100

    Obelisk

---
+++ Assets/obelisk.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obelisk : MonoBehaviour
+{
+    public SpawnManager spawnManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localScale = new Vector3(this.transform.localScale.x, 0.7f * spawnManager.zombiesSpawned / spawnManager.nbZombiesToSpawn, this.transform.localScale.z);
+    }
+}
